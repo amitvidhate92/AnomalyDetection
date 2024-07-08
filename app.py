f904e828a59328541fd5d0f25b1677bf4f9dc2ab
amitvidhate92 <156519844+amitvidhate92@users.noreply.github.com>
2024-07-08 09:51:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,20 @@ def predict_anomaly(CEACC,CCDEF,FAXAE,FBFFD,EDDAB):
 
 def main():   
     
-    st.title('Machine Learning Model Deployment with Streamlit')
+    st.title('Machine Learning Model Deployment with Streamlit ')
 
     html_temp="""
     <div style="background-color:tomato;padding:10px">
-    <h2 style ="color:white;text-algn:center;">Anamoly detection
+    <h2 style ="color:white;text-algn:center;">Anamoly detection(-1:Anomly Found , 1:No Anomly)
     </div>
     """
     
     st.markdown(html_temp,unsafe_allow_html=True)
-    CEACC=st.number_input("CEACC",0.0)
-    CCDEF=st.number_input("CCDEF",0.0)
-    FAXAE=st.number_input("FAXAE",0.0)
-    FBFFD=st.number_input("FBFFD",0.0)
-    EDDAB=st.number_input("EDDAB",0.0)
+    CEACC=st.number_input("CEACC",min_value=-1000.0, value=0.0)
+    CCDEF=st.number_input("CCDEF",min_value=-1000.0, value=0.0)
+    FAXAE=st.number_input("FAXAE",min_value=-1000.0, value=0.0)
+    FBFFD=st.number_input("FBFFD",min_value=-1000.0, value=0.0)
+    EDDAB=st.number_input("EDDAB",min_value=-1000.0, value=0.0)
     result=""
 
     if st.button("Predict"):
